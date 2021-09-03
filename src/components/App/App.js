f113bdfd85276e818b3972b4ca07955e715090ef
John <jashby@student.neumont.edu>
2021-09-03 17:55:32 -0600

    init setup

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App/App.css";
+import PhoneImg from "../../assets/images/phone.jpg";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Electron!</h1>
+      <div>Testing Wwwoooow</div>
+      <p>test test test</p>
+      <img src={PhoneImg} alt="images" />
+    </div>
+  );
+}
+
+export default App;
